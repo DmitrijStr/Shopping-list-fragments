d01f7fc515b7d15f03d75f29818c5be1c724d717
dmitrStrezh <s.d144@yanex.by>
2024-05-03 14:45:34 +0300

    [+] Added list w LinearLayout

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,17 +1,33 @@
 package com.example.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
 
 object ShopListRepositoryImpl : ShopListRepository {
-    private val shopList = mutableListOf<ShopItem>()
+
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
+    private val shopList = sortedSetOf<ShopItem>(object : Comparator<ShopItem> {
+        override fun compare(o1: ShopItem, o2: ShopItem): Int {
+            return o1.id - o2.id
+        }
+    })
+
     private var currId = 0
+
+    init {
+        repeat(100) {
+            addShopItem(ShopItem("Item $it", 1, true))
+        }
+    }
 
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFINED_ID) {
-            shopItem.id = currId
+            shopItem.id = currId++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun editShopItem(item: ShopItem) {
@@ -21,14 +37,20 @@ object ShopListRepositoryImpl : ShopListRepository {
     }
 
     override fun getShopItem(id: Int): ShopItem {
-        return shopList.find { it.id == id } ?: throw RuntimeException("Element with id: $id not found")
+        return shopList.find { it.id == id }
+            ?: throw RuntimeException("Element with id: $id not found")
     }
 
     override fun removeShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList() {
+        shopListLD.value = shopList.toList()
     }
 }
--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -1,21 +1,51 @@
 package com.example.shoppinglist.presentation
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var llListView: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        llListView = findViewById(R.id.ll_layout)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            showList(it)
+        }
+    }
+
+    fun showList(list: List<ShopItem>) {
+        llListView.removeAllViews()
+
+        for (item in list) {
+            val layoutId = if (item.isEnabled) R.layout.item_shop_enabled else R.layout.item_shop_disabled
+
+            val view = LayoutInflater.from(this).inflate(layoutId, llListView, false)
+            val name = view.findViewById<TextView>(R.id.tv_name)
+            val cnt = view.findViewById<TextView>(R.id.tv_count)
+            name.text = item.name
+            cnt.text = item.count.toString()
+
+            view.setOnLongClickListener {
+                viewModel.changeEnabledState(item)
+                true
+            }
+
+            llListView.addView(view)
         }
     }
 }
